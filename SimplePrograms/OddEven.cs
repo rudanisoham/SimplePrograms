eddc40a5bdf797aeb49cf53c076aa0c00c4474ac
Soham Rudani <Soham Rudani@SOHAM>
2025-07-04 11:07:36 +0530

    Add project files.

---
+++ SimplePrograms/OddEven.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePrograms
+{
+    internal class OddEven
+    {
+        static void Mian(string[] args)
+        {
+            Console.Write("Enter Number: ");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+
+            }
+    }
+}
---
+++ SimplePrograms/Profile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePrograms
+{
+    internal class Profile
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name : Rudani Soham J.");
+            Console.WriteLine("DOB : ");
+            Console.WriteLine("");
+        }
+    }
+}
